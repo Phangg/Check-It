fac079f0a855b8eaef9fe5ec314b971de012a8ac
Phangg <leeckdwns9305@gmail.com>
2025-01-10 15:31:20 +0900

    ✨ Add: 프로젝트 생성
    초기 모델 규모가 작기 때문에,
    Tuist 는 추후 도입 고민

---
+++ CHECKIT/CHECKIT/CHECKITApp.swift
@@ -0,0 +1,17 @@
+//
+//  CHECKITApp.swift
+//  CHECKIT
+//
+//  Created by phang on 1/10/25.
+//
+
+import SwiftUI
+
+@main
+struct CHECKITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
