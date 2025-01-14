d03f368bcc6a167eb171049ec78f77bceeaa2283
Phangg <leeckdwns9305@gmail.com>
2025-01-15 00:47:34 +0900

    🩹 Refactor: AppDelegate 적용 및 시작 화면 조정
    온보딩 후에 메인으로 화면 띄우도록 코드 수정

--- CHECKIT/CHECKIT/App/CHECKITApp.swift
+++ CHECKIT/CHECKIT/App/CHECKITApp.swift
@@ -9,9 +9,19 @@ import SwiftUI
 
 @main
 struct CHECKITApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) private var appDelegate
+    
+    @AppStorage(AppStorageKeys.isFirstOnboarding) private var isFirstOnboarding: Bool = true
+    
     var body: some Scene {
         WindowGroup {
-            MainView()
+            if isFirstOnboarding {
+                // 앱 최초 실행 시, 온보딩화면을 띄우기
+                OnboardingView()
+            } else {
+                // 메인 화면
+                MainView()
+            }
         }
     }
 }
