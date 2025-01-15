650c47971c8b1f180491cb829898150ffbb88989
Phangg <leeckdwns9305@gmail.com>
2025-01-15 16:50:05 +0900

    ✨ Add: Firebase 추가
    SPM 으로 추가
    
    AppDelegate 파일에서 FirebaseApp 연결

--- CHECKIT/CHECKIT/App/AppDelegate/AppDelegate.swift
+++ CHECKIT/CHECKIT/App/AppDelegate/AppDelegate.swift
@@ -7,12 +7,17 @@
 
 import UIKit
 
+import FirebaseCore
+
 class AppDelegate: NSObject, UIApplicationDelegate {
 
     func application(
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil
     ) -> Bool {
+        //
+        FirebaseApp.configure()
+        //
         registerDependencies()
         return true
     }
