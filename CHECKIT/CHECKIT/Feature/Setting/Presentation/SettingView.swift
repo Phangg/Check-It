518f1f2e35a300c018e3ee869c704d5d82a35c70
Phangg <leeckdwns9305@gmail.com>
2025-01-15 19:01:23 +0900

    ✨ Add: 화면 파일만 추가
    SettingView, SplashView

---
+++ CHECKIT/CHECKIT/Feature/Setting/Presentation/SettingView.swift
@@ -0,0 +1,20 @@
+//
+//  SettingView.swift
+//  CHECKIT
+//
+//  Created by phang on 1/10/25.
+//
+
+import SwiftUI
+
+struct SettingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
---
+++ CHECKIT/CHECKIT/Feature/Splash/Presentation/SplashView.swift
@@ -0,0 +1,20 @@
+//
+//  SplashView.swift
+//  CHECKIT
+//
+//  Created by phang on 1/10/25.
+//
+
+import SwiftUI
+
+struct SplashView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
