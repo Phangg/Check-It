6d74f68af1acd59b19ad9f3b1080f9b537e785d8
Phangg <leeckdwns9305@gmail.com>
2025-01-26 22:38:29 +0900

    🩹 Refactor: WebView 코드 수정
    WebView: UIViewRepresentable
    - 에러 메세지, webView 시트 열기 상태 변수 를 바인딩
    - Coordinator 로 Delegate 사용하여, 화면 상태에 따라 에러메세지 초기화 및 에러 띄우기 위한 메세지 저장 & 시트 닫기

--- CHECKIT/CHECKIT/DesignSystem/UIViewRepresentables/WebView.swift
+++ CHECKIT/CHECKIT/DesignSystem/UIViewRepresentables/WebView.swift
@@ -9,16 +9,63 @@ import SwiftUI
 import WebKit
 
 struct WebView: UIViewRepresentable {
-    let url: URL
+    @Binding private var errorMessage: String
+    @Binding private var showWebViewSheet: Bool
+    //
+    private let url: URL
 
+    init(
+        url: URL,
+        errorMessage: Binding<String>,
+        showWebViewSheet: Binding<Bool>
+    ) {
+        self.url = url
+        self._errorMessage = errorMessage
+        self._showWebViewSheet = showWebViewSheet
+    }
+    
     func makeUIView(context: Context) -> WKWebView {
         let webview = WKWebView()
-        webview.load(URLRequest(url: url))
+        webview.navigationDelegate = context.coordinator
+        let request = URLRequest(url: url)
+        webview.load(request)
         webview.allowsBackForwardNavigationGestures = true
         return webview
     }
 
-    func updateUIView(_ uiView: WKWebView, context: Context) {
-        //
+    func updateUIView(_ uiView: WKWebView, context: Context) { }
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    class Coordinator: NSObject, WKNavigationDelegate {
+        var parent: WebView
+        
+        init(_ parent: WebView) {
+            self.parent = parent
+        }
+        
+        func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
+        // 새로운 로딩 시작 시 상태 초기화
+            parent.errorMessage = ""
+        }
+        
+        // 웹뷰가 콘텐츠 데이터를 받아오는 것을 모두 마쳤을 때 호출
+        func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
+            parent.errorMessage = ""
+        }
+        
+        // 초기 로딩 에러
+        func webView(_ webView: WKWebView, didFailProvisionalNavigation navigation: WKNavigation!, withError error: Error) {
+            parent.errorMessage = error.localizedDescription
+            parent.showWebViewSheet = false
+        }
+        
+        // 초기 로딩 이후 에러
+        func webView(_ webView: WKWebView, didFail navigation: WKNavigation!, withError error: Error) {
+            parent.errorMessage = error.localizedDescription
+            parent.showWebViewSheet = false
+        }
     }
 }
