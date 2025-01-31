e38018bed289d19419841b9c25fb75153cc52928
Phangg <leeckdwns9305@gmail.com>
2025-01-31 22:05:25 +0900

    ✨ Add: 앱 메인 색상 변경 시, 앱 아이콘도 변경
    AppIcon 폴더에 색상별 png 등록 (1024*1024)
    
    AppMainColorManager
    - 기존 앱 메인 색상 변경 매니저에 코드 추가
    - init 할 때, fetchIcons 메서드를 통해 에셋 이름 info.plist 읽어와서 icons 배열에 저장
    - 앱 색상 변경할 때, 앱 아이콘도 변경되도록 내부에서 사용하는 updateAppIcon 메서드 작성
    - 현재 파일 안에서만 사용되는 Icon 구조체 작성 (fileprivate)

--- CHECKIT/CHECKIT/Common/Managers/AppMainColorManager.swift
+++ CHECKIT/CHECKIT/Common/Managers/AppMainColorManager.swift
@@ -16,16 +16,72 @@ final class AppMainColorManager: ObservableObject {
     private var appColorBrightness: Double = 0
     
     @Published var appMainColor: AppMainColor = .init(brightness: 0, baseColor: .accent)
-    
+    private var icons: [Icon] = []
     
     private init() {
         // AppStorage에 저장된 초기 색상으로 설정
         self.appMainColor = .init(brightness: appColorBrightness, baseColor: storedAppColor)
+        // AppIcon 에셋들 가져오기
+        fetchIcons()
     }
     
     func updateAppMainColor(to color: AppMainColor) {
         appMainColor = color
         appColorBrightness = color.brightness
         storedAppColor = color.baseColor
+        updateAppIcon(to: color.baseColor)
     }
+    
+    private func updateAppIcon(to baseColor: Color) {
+        guard UIApplication.shared.supportsAlternateIcons else {
+            print("Error: AppIcon - 동적 변경 실패")
+            return
+        }
+        let baseColorName = baseColor.description.capitalized
+        let newIconName = icons.first { $0.iconName == baseColorName }?.iconName ?? icons.first?.iconName
+        UIApplication.shared.setAlternateIconName(newIconName) { error in
+            if let error {
+                print(error.localizedDescription) // TODO: 에러 핸들링 수정 예정
+            }
+        }
+    }
+    
+    private func fetchIcons() {
+        if let bundleIcons = Bundle.main.object(forInfoDictionaryKey: "CFBundleIcons") as? [String: Any] {
+            // Default Icon
+            if let primaryIcon = bundleIcons["CFBundlePrimaryIcon"] as? [String: Any],
+               let iconFileName = (primaryIcon["CFBundleIconFiles"] as? [String])?.first {
+                let displayName = (primaryIcon["CFBundleIconName"] as? String) ?? ""
+                icons.append(
+                    Icon(
+                        displayName: displayName,
+                        iconName: nil,
+                        image: UIImage(named: iconFileName)
+                    )
+                )
+            }
+            // Alternate Icons
+            if let alternateIcons = bundleIcons["CFBundleAlternateIcons"] as? [String: Any] {
+                alternateIcons.forEach { iconName, iconInfo in
+                    if let iconInfo = iconInfo as? [String: Any],
+                       let iconFileName = (iconInfo["CFBundleIconFiles"] as? [String])?.first {
+                        icons.append(
+                            Icon(
+                                displayName: iconName,
+                                iconName: iconName,
+                                image: UIImage(named: iconFileName)
+                            )
+                        )
+                    }
+                }
+            }
+        }
+    }
+}
+
+// AppIcon
+fileprivate struct Icon {
+    let displayName: String
+    let iconName: String?
+    let image: UIImage?
 }
